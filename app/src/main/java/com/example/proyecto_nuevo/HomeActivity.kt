2d44cc2e27b9866ad9bb68e0356cbe4973ad6e8b
Braulio <braulioarguetamartinez3c2bda@gmail.com>
2021-06-08 09:55:22 -0500

    Version 2

--- app/src/main/java/com/example/proyecto_nuevo/HomeActivity.kt
+++ app/src/main/java/com/example/proyecto_nuevo/HomeActivity.kt
@@ -9,6 +9,7 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_auth.*
 import kotlinx.android.synthetic.main.activity_home.*
@@ -21,11 +22,12 @@ enum class ProviderType{
 
 class HomeActivity : AppCompatActivity() {
 
-    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+
 
         val bundle :Bundle?=intent.extras
         val email :String?=bundle?.getString("email")
@@ -37,39 +39,21 @@ class HomeActivity : AppCompatActivity() {
         prefs.putString("provider",provider)
         prefs.apply()
 
-        toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open, R.string.close)
-        drawerLayout.addDrawerListener(toggle)
 
-        toggle.syncState()
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        navView.setNavigationItemSelectedListener {
-            when(it.itemId){
-                R.id.miItem1 -> Toast.makeText(applicationContext,"Click 1", Toast.LENGTH_SHORT).show()
-                R.id.miItem2 -> Toast.makeText(applicationContext,"Click 2", Toast.LENGTH_SHORT).show()
-                R.id.miItem3 -> Toast.makeText(applicationContext,"Click 3", Toast.LENGTH_SHORT).show()
-            }
-            true
 
-        }
+
 
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (toggle.onOptionsItemSelected(item)){
-            return true;
 
-        }
-        return super.onOptionsItemSelected(item)
-    }
 
     private fun setup(email:String, provider:String){
 
         title="Inicio"
        // emailTextView.text=email
         //providerTextView.text=provider
-
 
         logOutButton.setOnClickListener {
 
